12e900c12aaf19dfa7ec38a530e015d155b68203
maruzhang <maruzhang@users.noreply.github.com>
2020-07-10 14:55:32 +0900

    update installer

--- Installer.sh
+++ Installer.sh
@@ -2,5 +2,8 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
