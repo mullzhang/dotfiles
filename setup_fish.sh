68670138249a81daf3d8e499dfc9d8a4588a2361
maruzhang <maruzhang@users.noreply.github.com>
2020-07-11 04:17:22 +0900

    remove fisher theme

--- setup_fish.sh
+++ setup_fish.sh
@@ -4,6 +4,5 @@
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
 # fisher plugins
-fisher add oh-my-fish/theme-agnoster
 fisher add oh-my-fish/plugin-peco
 fisher add decors/fish-ghq
