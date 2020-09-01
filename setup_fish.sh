43d893079dfd243ddac84782e9cf7622e77e5b62
maruzhang <maruzhang@users.noreply.github.com>
2020-09-01 16:15:22 +0900

    add z command

--- setup_fish.sh
+++ setup_fish.sh
@@ -6,3 +6,4 @@ curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fi
 # fisher plugins
 fisher add oh-my-fish/plugin-peco
 fisher add decors/fish-ghq
+fisher add jethrokuan/z
