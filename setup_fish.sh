7326fae6ef0fd842d65e8eb9898d4b737bbc3275
mullzhang <mullzhang@users.noreply.github.com>
2020-09-22 21:15:42 +0900

    fix setup script

--- setup_fish.sh
+++ setup_fish.sh
@@ -4,6 +4,7 @@
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
 # fisher plugins
+fisher add 0rax/fish-bd
 fisher add decors/fish-ghq
 fisher add jethrokuan/z
 fisher add oh-my-fish/plugin-peco
