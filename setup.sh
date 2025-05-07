c0feb608c2239484db55b9dd7a75bb77290c1761
mullzhang <mullzhang@users.noreply.github.com>
2025-05-07 23:43:04 +0900

    remove symbolic links to global gitignore file

--- setup.sh
+++ setup.sh
@@ -11,4 +11,4 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.gitignore_global ~/.config/git/ignore
